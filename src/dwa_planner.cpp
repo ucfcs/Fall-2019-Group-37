8429173c349dc74939e05e8b7e4dc8a7c9005287
Joehill27 <josephmhill96@gmail.com>
2020-02-28 23:30:36 -0500

    Add various testing for global planner, and dwa local planner creation code.

--- src/dwa_planner.cpp
+++ src/dwa_planner.cpp
@@ -2,7 +2,6 @@
 #include <costmap_2d/costmap_2d_ros.h>
 #include <dwa_local_planner/dwa_planner_ros.h>
 
-...
 
 tf::TransformListener tf(ros::Duration(10));
 costmap_2d::Costmap2DROS costmap("costmap", tf);
