739c55ee427cf56a82595cafdae85a1f4c260599
Group 37 <GranoblasticMan@users.noreply.github.com>
2020-03-02 15:22:24 -0500

    Fixed wheels (sort-of)

--- egoat.ino
+++ egoat.ino
@@ -12,92 +12,133 @@
  *  Replacing /dev/ttyACM0 with the serial port of the Arduino device
  *  and using the desired values for the left and right motor speeds
  */
-
+ 
+#include <Arduino.h>
+#include <Wire.h>
+#include <MPU9250.h>
 #include <ros.h>
-#include "egoat/SetMotorSpeed.h"
+#include "ros/time.h"
+#include "geometry_msgs/Twist.h"
+#include "tf/transform_broadcaster.h"
+#include "nav_msgs/Odometry.h"
 
 //Motor Pins
-const int EN_A = 11;      //Enable pin for first motor
-const int IN1 = 9;       //control pin for first motor
-const int IN2 = 8;       //control pin for first motor
-const int IN3 = 7;        //control pin for second motor
-const int IN4 = 6;        //control pin for second motor
-const int EN_B = 10;      //Enable pin for second motor
+const int EN_R = 9;      //Enable pin for left motor
+const int IN1_R = 11;    //control pin for left motor
+const int IN2_R = 10;    //control pin for left motor
+
+const int IN1_L = 13;    //control pin for right motor
+const int IN2_L = 12;    //control pin for right motor
+const int EN_L = 8;      //Enable pin for right motor
+
+//Bot dimensions
+//TODO: Correct for final bot
+const double wheel_rad = 0.0325, wheel_sep = 0.140;
+const double max_rpm = 60;
+
+
+const double max_wheel_vel = TWO_PI * wheel_rad * (max_rpm / 60.0);
+
+//Wheel velocities
+double wheel_r = 0.0, wheel_l = 0.0;
 
 ros::NodeHandle  nh;
 
+//IMU declaration
+MPU9250 IMU(Wire,0x68);
+
+void stopAllMotors();
+
 /*
- * Handle the message received.
+ * Receive the Twist message and convert it into motor controller
+ * commands.
  */
-void messageCb(const egoat::SetMotorSpeed& msg){
-  int left = msg.left_motor;
-  int right = msg.right_motor;
-
-  // Convert message to values the motor controller understands
-  writeMotorSpeedAndDirection(left, EN_A, IN1, IN2);
-  writeMotorSpeedAndDirection(right, EN_B, IN3, IN4);
-
-  // Use NodeHandle to output debug info to rostopic
-  char buf[5]; // loginfo is picky and wants a char*, so we have to use itoa
-  nh.loginfo("Received: ");
-  nh.loginfo(itoa(left, buf, 10));
-  nh.loginfo(itoa(right,buf, 10));
+void messageCbTwist( const geometry_msgs::Twist& msg){
+  double speed_ang = msg.angular.z;
+  double speed_lin = msg.linear.x;
+  
+  
+  if(speed_lin > max_wheel_vel)
+    speed_lin = max_wheel_vel;
+  
+  wheel_r = (speed_lin/wheel_rad) + ((speed_ang*wheel_sep)/(2.0*wheel_rad));
+  wheel_l = (speed_lin/wheel_rad) - ((speed_ang*wheel_sep)/(2.0*wheel_rad));
 }
 
-/*
- * Convert the message value
- * 
- * Currently, the ROS message SetMotorSpeed receives 2 int8, which means
- * the range for each motor is [-128, 127].
- * 
- * The intention is to set the motor speed as a percentage (in int)
- * of maximum motor speed. This will have to be mapped to
- * [0, 255] for the L298N motor controller on the prototype's
- * speed, and the sign indicates the direction
- * 
- * TODO: Check the range of values for the final version's
- * motor controllers.
- */
-void writeMotorSpeedAndDirection(int messageValue, int enablePin, int input1, int input2) {
-  int powerOut = map(abs(messageValue), 0, 100, 0, 255);
-  analogWrite(enablePin, powerOut);
-  char buf[10];
-    nh.loginfo("analogWrite:");
-    nh.loginfo("enablePin:");
-    nh.loginfo(itoa(enablePin, buf, 10));
-    nh.loginfo("powerOut:");
-    nh.loginfo(itoa(powerOut, buf, 10));
-  if(messageValue > 0) {
-    digitalWrite(input1, HIGH);
-    digitalWrite(input2, LOW);
-    nh.loginfo("digitalWrite HIGH LOW");
-  } else if(messageValue < 0) {
-    digitalWrite(input1, LOW);
-    digitalWrite(input2, HIGH);
-    nh.loginfo("digitalWrite LOW HIGH");
+void wheel_l_pulse(int pulse) {
+  if (pulse > 0) {
+    analogWrite(EN_L, pulse);
+    digitalWrite(IN1_L, HIGH);
+    digitalWrite(IN2_L, LOW);
+  } else if (pulse < 0) {
+    pulse = abs(pulse);
+    analogWrite(EN_L, pulse);
+    digitalWrite(IN1_L, LOW);
+    digitalWrite(IN2_L, HIGH);
   } else {
-    digitalWrite(input1, LOW);
-    digitalWrite(input2, LOW);
-    
-    nh.loginfo("digitalWrite LOW LOW");
+    wheel_l_stop();
   }
 }
 
-ros::Subscriber<egoat::SetMotorSpeed> sub("motors", &messageCb);
+void wheel_r_pulse(int pulse) {
+  if (pulse > 0) {
+    analogWrite(EN_R, pulse);
+    digitalWrite(IN1_R, LOW);
+    digitalWrite(IN2_R, HIGH);
+  } else if (pulse < 0) {
+    pulse = abs(pulse);
+    analogWrite(EN_R, pulse);
+    digitalWrite(IN1_R, HIGH);
+    digitalWrite(IN2_R, LOW);
+  } else {
+    wheel_r_stop();
+  }
+}
+
+
+
+void wheel_l_stop() {
+  digitalWrite(EN_L, LOW);
+  digitalWrite(IN1_L, LOW);
+  digitalWrite(IN2_L, LOW);
+}
+
+void wheel_r_stop() {
+  digitalWrite(EN_R, LOW);
+  digitalWrite(IN1_R, LOW);
+  digitalWrite(IN2_R, LOW);
+}
+
+ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", &messageCbTwist);
+nav_msgs::Odometry odom_msg;
+//ros::Publisher odom_pub("odom", &odom_msg);
+tf::TransformBroadcaster odom_broadcaster;
+
+ros::Time current_time, last_time;
+char base_link[] = "/base_link";
+// char odom[] = "/odom";
 
 void setup()
 {   
   // Set up motor controller pins
-  pinMode(EN_A, OUTPUT);
-  pinMode(IN1, OUTPUT);  
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);  
-  pinMode(IN4, OUTPUT);
-  pinMode(EN_B, OUTPUT);
+  pinMode(EN_L, OUTPUT);
+  pinMode(IN1_L, OUTPUT);  
+  pinMode(IN2_L, OUTPUT);
+  pinMode(IN1_R, OUTPUT);  
+  pinMode(IN2_R, OUTPUT);
+  pinMode(EN_R, OUTPUT);
+  
+  stopAllMotors();
+
+  int status;
+  status = IMU.begin();
 
   // Set up the NodeHandler for ROS
   nh.initNode();
   nh.subscribe(sub);
+//  nh.advertise(odom_pub);
+
+  current_time = last_time = nh.now();
 }
 
 /*
@@ -107,5 +148,14 @@ void setup()
 void loop()
 {  
   nh.spinOnce();
+  
+  wheel_l_pulse(wheel_l*10);
+  wheel_r_pulse(wheel_r*10);
+  
   delay(1);
+}
+
+void stopAllMotors() {
+  wheel_l_stop();
+  wheel_r_stop();
 }
